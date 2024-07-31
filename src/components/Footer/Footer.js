6c20fca96ca4c3f49a45bc4a3fbca6b3628f0d6c
Pranita <pfulsundar8@gmail.com>
2024-07-30 18:55:59 -0700

    feat: header and footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footerContainer">
+      <h1>HACKERNEWS.</h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="headerContainer">
+      <h1 className="headerText">
+        <span className="highlightedText">HACKER</span>
+        NEWS
+        <span className="highlightedText">.</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
